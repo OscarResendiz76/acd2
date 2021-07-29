f40e07af8c965027234cddf758d7f143bef3fa56
OscarResendiz76 <87833572+OscarResendiz76@users.noreply.github.com>
2021-07-29 16:34:37 -0500

    Versio modificada

--- arteycultura/settings.py
+++ arteycultura/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)oaof$ym9-d3iqws002d@r$!1du05nk!f5))^+a$qvnci(w%0g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [".herokuapp.com"]
 
